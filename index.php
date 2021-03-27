06b99fe22ab6eca07479cac243358d71efc38f55
ermigr <74967366+ermigr@users.noreply.github.com>
2021-03-27 09:03:50 +0200

    Работа с массивом.

--- index.php
+++ index.php
@@ -1,56 +1,47 @@
 <?php
-//Запрос на создание таблицы
-$create="CREATE TABLE `test` (
-  `ID` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `NAME` text NOT NULL,
-  `DATA_INSERT` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-)";
-//Запрос на внесение записей
-$insert="INSERT INTO `test` (`ID`, `NAME`, `DATA_INSERT`) VALUES
-(null, 'Ivanov', default),
-(null, 'Petrov', default),
-(null, 'Sidorov', default);";
+$data = [
+    'question' => ['почему', 'как', 'зачем', 'столько'],
+    'animals' => [
+        'birds' => [
+            [
+                'name' => 'грачи',
+            ],
+            [
+                'name' => 'воробьи',
+            ],
+        ],
+        'others' => [
+            [
+                ['name' => 'кошки'],
+                ['name' => 'рыбы'],
+                ['name' => 'собаки'],
+            ],
+        ],
+    ],
+    'parts' => [
+        'hands' => 'рук',
+        'feathers' => 'перьев',
+        'eyes' => 'глаз',
+    ],
+];
 
-//Создаем функцию для работы с таблицей
-function sql($query) {
-	define('SERVER','localhost');
-	define('LOGIN','root');
-	define('PASS','root');
-	define('DB','lesson');
-	$connect=mysqli_connect(SERVER,LOGIN,PASS,DB);    //Подключение к базе данных
-
-	return mysqli_query($connect,$query);
-}
-//Создаем таблицу, вносим записи
-sql($create);
-sql($insert);
-
-//Выводим данные
-//Вариант №1-вывод по названию поля
-$arResult=[];
-$query='select*from test';
-$sql=sql($query);
-//Получение ассоциативного массива из MySQL
-while ($res=mysqli_fetch_assoc($sql)) {
-	foreach($res as $key=>$item) {
-		if (!$arResult[$key]) {
-			$arResult[$key]=[];
-		}
-		array_push($arResult[$key],$item);
-	}
-}
-print_r($arResult);    //Ctrl-U для наглядности
-
-//Вариант №2-вывод по записям
-$arResult=[];
-$query='select*from test';
-$sql=sql($query);
-while ($res=mysqli_fetch_assoc($sql)) {
-	$arResultItem=[];
-	foreach($res as $key=>$item) {
-		$arResultItem[$key]=$item;
-	}
-	array_push($arResult,$arResultItem);
-}
-print_r($arResult);
-?>
+/* почему грачи не кошки и зачем им столько перьев */
+echo $data['question'][0];
+echo ' ';
+echo $data['animals']['birds'][0]['name'];
+echo ' ';
+echo array_search ('грачи', $data['animals']['birds'][0])[0];
+echo substr($data['question'][0],6,2);
+echo ' ';
+echo $data['animals']['others'][0][0]['name'];
+echo ' ';
+echo substr($data['animals']['birds'][0]['name'],8,2);
+echo ' ';
+echo $data['question'][2];
+echo ' ';
+echo substr($data['animals']['birds'][0]['name'],8,2);
+echo substr($data['question'][2],8,2);
+echo ' ';
+echo $data['question'][3];
+echo ' ';
+echo $data['parts']['feathers'];
